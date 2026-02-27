b7cb1f6bec554641024582865326ec533a7dfb03
Sairam Golakoti <sairam.g581@gmail.com>
2026-02-27 16:25:14 +0100

    Test case with N=8

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int sc_main(int argc, char* argv[]) {
     std::cout << "Starting Interleaved FFT Simulation (Refactored)" << std::endl;
 
     // Parameters: N=8, Cores=2, Hop=1
-    Testbench<4, 2, 1> tb("tb");
+    Testbench<8, 2, 1> tb("tb");
     
     // Run Simulation
     sc_start();
